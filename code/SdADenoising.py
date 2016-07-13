c66593aaf7f37849fbc95a44109ebcaca093583d
alcor <alcor@dtu.dk>
2016-07-13 17:26:08 +0200

    test code

--- code/SdADenoising.py
+++ code/SdADenoising.py
@@ -490,4 +490,17 @@ def test_SdA(finetune_lr=0.01, pretraining_epochs=100,
 #    d2, cost = get_cost(noisy_datasets, clean_datasets, sda)
 #    # end-snippet-4
 if __name__ == "__main__":
-    test_SdA(finetune_lr=0.01, pretraining_epochs=100, pretrain_lr=0.01, training_epochs=100, hidden_layers_fraction = [0.5, 0.5, 0.5], noise_dataset_samples = 5,  batch_size = 128)
+    pretrain_epochs = [10000]
+    finetune_rates = [0.01, 0.1]
+    pretrain_rates = [0.01, 0.1]
+    finetune_epochs = [10000]    
+    hl = [[0.5, 0.5, 0.5]]
+    batch_sizes = [128, 256]
+
+    for b in batch_sizes:
+        for l in hl:
+            for lr2 in finetune_rates:
+                for lr1 in pretrain_rates:
+                    for e1 in pretrain_epochs:
+                        for e2 in finetune_epochs:                       
+                            test_SdA(finetune_lr=lr2, pretraining_epochs=e1, pretrain_lr=lr1, training_epochs=e2, hidden_layers_fraction = l, noise_dataset_samples = 5,  batch_size = b)
